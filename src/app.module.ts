3c42d3d4d396d46654c1957967a529bd337354b7
ilcann <ilcan21@itu.edu.tr>
2025-10-29 16:15:58 +0300

    refactor(module): app.module.ts - proxy wiring changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { ProxyModule } from './proxy/proxy.module';
+import { appConfig } from './config/app.config';
+import { servicesConfig } from './config/services.config';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration, appConfig, servicesConfig],
+      isGlobal: true,
+    }),
+    ProxyModule,
+  ],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
