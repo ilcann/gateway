2a8d873bb57f829818e802868ca3a3a4a03f705e
ilcann <ilcan21@itu.edu.tr>
2025-10-29 16:15:51 +0300

    feat(server): main.ts güncellemesi — start/ts changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
+import { AppConfig } from './config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(cookieParser());
+
+  const configService = app.get(ConfigService);
+  const appConfig = configService.get<AppConfig>('app');
+
+  app.setGlobalPrefix(appConfig!.globalPrefix);
+
+  await app.listen(appConfig!.port, appConfig!.host);
+  console.log(`Application "${appConfig!.name}" is running on: http://${appConfig!.host}:${appConfig!.port}/${appConfig!.globalPrefix}/`);
 }
 bootstrap();
