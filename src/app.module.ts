c2626d0bd8d5585816856b8a973b6db3311b49cd
ilcann <ilcan21@itu.edu.tr>
2025-11-02 01:58:30 +0300

    refactor(app): app.module.ts - yeni modül yapısı entegrasyonu

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import configuration from './config/configuration';
-import { ProxyModule } from './proxy/proxy.module';
+import { ProxyModule } from './modules/proxy/proxy.module';
 import { appConfig } from './config/app.config';
 import { servicesConfig } from './config/services.config';
 import { AppController } from './app.controller';
+import { AuthModule } from './modules/auth/auth.module';
+import { jwtConfig } from './config/jwt.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [configuration, appConfig, servicesConfig],
+      load: [appConfig, jwtConfig, servicesConfig],
       isGlobal: true,
     }),
     ProxyModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [],
