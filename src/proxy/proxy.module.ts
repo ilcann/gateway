7c9766c31c602d154d37f316427e1358e50c1655
ilcann <ilcan21@itu.edu.tr>
2025-11-02 01:58:07 +0300

    refactor(proxy): eski proxy yapısı kaldırıldı (proxy-auth.controller ve proxy.module)

--- src/proxy/proxy.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProxyAuthController } from './proxy-auth.controller';
-
-@Module({
-  controllers: [ProxyAuthController],
-})
-export class ProxyModule {}
