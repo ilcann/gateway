bbee0af5fb0fde90a0642adc0bb4f1c70572c5e0
ilcann <ilcan21@itu.edu.tr>
2025-11-11 20:48:37 +0300

    feat(notification): enhance NotificationGateway to validate access tokens and add NotificationProcessor to module

--- src/modules/notification/notification.gateway.ts
+++ src/modules/notification/notification.gateway.ts
@@ -1,4 +1,4 @@
-import { UseGuards } from '@nestjs/common';
+import { Logger, UseGuards } from '@nestjs/common';
 import {
   WebSocketGateway,
   WebSocketServer,
@@ -9,21 +9,28 @@ import {
 import { Server, Socket } from 'socket.io';
 import { WsJwtAuthGuard } from '../auth/guards/ws-jwt-auth.guard';
 import { CurrentUserWs, type RequestUser } from '@tssx-bilisim/praiven-contracts';
+import { AuthService } from '../auth/auth.service';
 
-@WebSocketGateway({ namespace: '/notifications' })
+@WebSocketGateway()
 export class NotificationGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
-
+  private readonly logger = new Logger(NotificationGateway.name);
+  constructor(private readonly authService: AuthService) {}
+  
   afterInit() {
     console.log('ChatGateway initialized');
   }
 
-  @UseGuards(WsJwtAuthGuard) // Burada guard kullanıyoruz
-  async handleConnection(@CurrentUserWs() user: RequestUser, client: Socket) {
-    // guard başarılıysa client.user Passport strategy validate() çıktısını içerir
-    client.data = { user };
-    await client.join(user.userId);
+  async handleConnection(client: Socket) {
+    const token = client.handshake.auth.token;
+    if (!token) {
+      this.logger.warn(`Client connection rejected: Missing access token`);
+      client.disconnect(true);
+      return;
+    }
+    const user: RequestUser = await this.authService.validateAccessToken(token);
 
+    await client.join(user.userId);
     console.log(`Client connected: ${client.id}`);
   }
 
--- src/modules/notification/notification.module.ts
+++ src/modules/notification/notification.module.ts
@@ -3,13 +3,16 @@ import { Module } from '@nestjs/common';
 import { QueueNames } from '@tssx-bilisim/praiven-contracts';
 import { NotificationGateway } from './notification.gateway';
 import { NotificationService } from './notification.service';
+import { NotificationProcessor } from './processors/message.processor';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     BullModule.registerQueue({
       name: QueueNames.MESSAGE_NOTIFICATION_QUEUE
     }),
+    AuthModule
   ],
-  providers: [NotificationGateway, NotificationService]
+  providers: [NotificationGateway, NotificationService, NotificationProcessor]
 })
 export class NotificationModule {}
